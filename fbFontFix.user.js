cddc3a12db87d0e0eaf14e22499b57598fbe9eb1
Lê Hoàng <CryToCry96@users.noreply.github.com>
2020-09-23 23:19:22 +0700

    Create fbFontFix.user.js

---
+++ fbFontFix.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Facebook new theme font fix
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Facebook new theme font fix
+// @author       Le Hoang
+// @match        https://*.facebook.com/
+// ==/UserScript==
+function addGlobalStyle(css) {
+    var body, style;
+    body = document.getElementsByTagName('body')[0];
+    if (!body) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    body.appendChild(style);
+}
+
+addGlobalStyle(
+    `* {font-family: "Segoe UI", Arial, sans-serif !important;
+}
+.rrkovp55 { font-family: "Segoe UI", Arial, sans-serif !important; }
+`);
